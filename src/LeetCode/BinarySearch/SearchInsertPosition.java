5c3bba9a7dd42f1060da340623d9c2f6ec7983ba
Dmitriy Pushkarev <dum3712@mail.ru>
2023-09-23 20:07:34 +0400

    Add LeetCode problems №35

---
+++ src/LeetCode/BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,35 @@
+package LeetCode.BinarySearch;
+
+public class SearchInsertPosition {
+    public static int searchInsert(int[] nums, int target) {
+        int lowerBound=0;
+        int upperBound=nums.length-1;
+        int mid;
+        while(true){
+            mid=(lowerBound+upperBound)/2;
+            if(nums[mid]==target){
+                return mid;
+            } else if(lowerBound>upperBound){
+                return lowerBound;
+            }else{
+                if(nums[mid]<target){
+                    lowerBound=mid+1;
+                }else{
+                    upperBound=mid-1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+         int[] arr=new int[4];
+         arr[0]=1;
+         arr[1]=4;
+         arr[2]=9;
+         arr[3]=10;
+        System.out.println(searchInsert(arr,8));
+
+
+    }
+}
